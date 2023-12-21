22d24b15d649c27727b48f07aaf478c983fffdb0
jasper <jasper@mail.de>
2023-12-21 16:16:25 +0100

    comment buffered_write_closer

--- buffered_write_closer.go
+++ buffered_write_closer.go
@@ -1,26 +1,26 @@
 package main
 
-import (
-	"bufio"
-	"io"
-)
+// import (
+// 	"bufio"
+// 	"io"
+// )
 
-type bufferedWriteCloser struct {
-	*bufio.Writer
-	io.Closer
-}
+// type bufferedWriteCloser struct {
+// 	*bufio.Writer
+// 	io.Closer
+// }
 
-// NewBufferedWriteCloser creates an io.WriteCloser from a bufio.Writer and an io.Closer
-func NewBufferedWriteCloser(writer *bufio.Writer, closer io.Closer) io.WriteCloser {
-	return &bufferedWriteCloser{
-		Writer: writer,
-		Closer: closer,
-	}
-}
+// // NewBufferedWriteCloser creates an io.WriteCloser from a bufio.Writer and an io.Closer
+// func NewBufferedWriteCloser(writer *bufio.Writer, closer io.Closer) io.WriteCloser {
+// 	return &bufferedWriteCloser{
+// 		Writer: writer,
+// 		Closer: closer,
+// 	}
+// }
 
-func (h bufferedWriteCloser) Close() error {
-	if err := h.Writer.Flush(); err != nil {
-		return err
-	}
-	return h.Closer.Close()
-}
+// func (h bufferedWriteCloser) Close() error {
+// 	if err := h.Writer.Flush(); err != nil {
+// 		return err
+// 	}
+// 	return h.Closer.Close()
+// }
